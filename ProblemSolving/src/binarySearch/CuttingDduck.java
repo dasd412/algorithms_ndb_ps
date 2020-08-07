7975334f1de231502af36a66d960c9f62e0f2939
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-08-07 18:53:11 +0900

    CuttingDduck optimized in memory perspective

--- ProblemSolving/src/binarySearch/CuttingDduck.java
+++ ProblemSolving/src/binarySearch/CuttingDduck.java
@@ -15,18 +15,18 @@ public class CuttingDduck {
         int n=Integer.parseInt(splits[0]);
         long m=Long.parseLong(splits[1]);
 
-        long[]dducks=new long[n];
+        long[]trees=new long[n];
         splits=br.readLine().split(" ");
         for(int i=0;i<n;i++){
-            dducks[i]=Long.parseLong(splits[i]);
+            trees[i]=Long.parseLong(splits[i]);
         }
 
-        Arrays.sort(dducks);
+        Arrays.sort(trees);
 
-        long maxHeight=getMaxHeight(0,n-1,dducks,m);
+        long maxHeight=getMaxHeight(0,n-1,trees,m);
 
         bw.write(Long.toString(maxHeight)+"\n");
-        
+
         br.close();
         bw.close();
     }
@@ -36,42 +36,37 @@ public class CuttingDduck {
         long height=0;
 
 
-        long[]heights=new long[(int) (arr[end]+1)];//0부터 떡의 최댓값까지 가능한 길이들을 담는 배열..
-
         /*
         떡의 길이가 19, 15, 10, 17인 경우 최솟값은 10, 최댓값은 19다.
-        따라서 길이들을 담는 배열은 0,1,2,3....18,19를 담고 있다.
+        따라서 길이들의 범위는  0,1,2,3,4....18,19이다.
 
          */
-        for(int a=0;a<heights.length;a++){
-            heights[a]=a;
 
-        }
 
-        int i=0;
-        int j=heights.length-1;
+        long i=0;
+        long j=arr[end];
 
         long sum;
 
         while(i<=j){
 
-            int mid=(i+j)/2;
+            long mid=(i+j)/2;
 
             sum=0;
 
             for(int a=0;a<arr.length;a++){
-                if(arr[a]>heights[mid]){//mid 인덱스의 길이 값보다 큰 떡 길이에 대해
-                    sum=sum+arr[a]-heights[mid];//자를 길이의 합을 구한다.
+                if(arr[a]> mid){//mid 인덱스의 길이 값보다 큰 떡 길이에 대해
+                    sum=sum+arr[a]- mid;//자를 길이의 합을 구한다.
                 }
 
             }
 
             if(sum==target){//더한 값이 m과 같다면, 찾은 것이다.
-                return heights[mid];
+                return  mid;
             }
             else if(sum>target){//더한 값이 m보다 크다면,
 
-                height=heights[mid];//일단 적어도 m만큼의 떡은 얻으므로 길이를 저장한다.
+                height= mid;//일단 적어도 m만큼의 떡은 얻으므로 길이를 저장한다.
                 i=mid+1;//더한 값이 원하는 값보다 크므로 자르는 기준을 높인다.
             }
             else{//더한 값이 m보다 작다면,
