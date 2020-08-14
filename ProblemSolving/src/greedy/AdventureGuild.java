86ee5a2253a1e47c3bde13865edf79abda76a6a0
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-08-14 12:46:08 +0900

    AdventureGuild in greedy package updated. But this code is  code of ndb who is author of this book.

--- ProblemSolving/src/greedy/AdventureGuild.java
+++ ProblemSolving/src/greedy/AdventureGuild.java
@@ -1,6 +1,9 @@
 package greedy;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class AdventureGuild {
@@ -11,42 +14,32 @@ public class AdventureGuild {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n= Integer.parseInt(br.readLine());
-        int[]fears=new int[1000001];//모험가의 공포도 리스트
 
 
-
-        int max=0;
+        List<Integer> fears=new ArrayList<>();
 
         String[]splits=br.readLine().split(" ");
         for(int i=0;i<n;i++){
-            int index= Integer.parseInt(splits[i]);
-            fears[index]++;
-            max=Math.max(max,index);
+           fears.add (Integer.parseInt(splits[i]));
+
+
         }
+
+        Collections.sort(fears);
 
         int groupCount=0;
 
-        int i=1;
-        for(;i<=max;i++){
+        int count=0;
 
-            if(fears[i]>=i){//i의 공포도를 갖는 모험가가 i명 이상 존재하면
-
-                for(int j=fears[i];j>=i;){//i미만이 되기 전까지
-                    groupCount++;//그룹 하나를 만들고
-                    j-=i;//넣은 모험가들을 명단에서 뺀다.
-                }
-
-            }
-            else{//더 이상 i의 공포도를 갖는 모험가가 i명 이상 존재하지 않는다면, 나머지를 모아 처리할 수 있는지 확인한다.
-
-                if(fears[i]!=0&&fears[i+1]+fears[i]>=i+1){//i번째와 i+1번째 공포도를 갖는 모험가들의 합이 i+1명 이상이라면 한 그룹으로 모아 처리한다.
-                    groupCount++;
-                    fears[i+1]-=fears[i];
-                    fears[i]=0;
-                }
-
+        for(int i=0;i<fears.size();i++){
+            count++;
+            if(count>=fears.get(i)){
+                groupCount++;
+                count=0;
             }
         }
+
+
 
 
 
